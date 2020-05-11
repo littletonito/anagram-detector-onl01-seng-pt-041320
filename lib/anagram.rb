ea0e9833ac9ddb7181173a5c7afe0f8b019b0b11
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-05-11 01:23:12 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+
+class Anagram
+  
+  attr_accessor :name
+  
+  def initialize(anagram)
+    @anagram = anagram
+  end
+  
+  def match(array)
+    array.select {|a| a.split("").sort == @anagram.split("").sort}
+  end
+  
+end
