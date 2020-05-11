cc5e38dcdf0ac508147c376b42580deeb0a89332
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-05-11 01:22:30 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -9,7 +9,7 @@ class Anagram
   end
   
   def match(array)
-    array.select {|a| a.split("").sort == @anagram.split("").sort}
+    array.each {|a| a.split("").sort == @anagram.split("").sort}
   end
   
 end
